042b3790e74c939d3603233d8f34275656da56aa
ms-online <27732357@qq.com>
2021-05-23 17:04:24 +0800

    lesson-19

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,26 +1,84 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import classes from './contact-form.module.css'
+import Notification from '../ui/notification'
+async function sendContactData(contactDetails) {
+  const response = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(contactDetails),
+    headers: { 'Content-Type': 'application/json' },
+  })
+
+  const data = await response.json()
+  if (!response.ok) {
+    throw new Error(data.message || '出错了')
+  }
+}
+
 function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState('')
   const [enteredName, setEnteredName] = useState('')
   const [enteredMessage, setEnteredMessage] = useState('')
+  const [requestStatus, setRequestStatus] = useState() //'pending','success','error'
+  const [requestError, setRequestError] = useState()
+
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null)
+        setRequestError(null)
+      }, 3000)
+      return () => clearTimeout(timer)
+    }
+  }, [requestStatus])
 
   //可选项：在客户端做一个简单的数据验证
 
-  function sendMessageHandler(event) {
+  async function sendMessageHandler(event) {
     event.preventDefault()
-    fetch('/api/contact', {
-      method: 'POST',
-      body: JSON.stringify({
+    setRequestStatus('pending')
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+      })
+      setRequestStatus('success')
+      setEnteredEmail('')
+      setEnteredName('')
+      setEnteredMessage('')
+    } catch (error) {
+      setRequestError(error.message)
+      setRequestStatus('error')
+    }
   }
+
+  //弹框props
+  let notification
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pengding',
+      title: '发送中',
+      message: '你的留言信息正在传送中...',
+    }
+  }
+
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: '成功',
+      message: '你的留言信息发送成功！',
+    }
+  }
+
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: '失败',
+      message: requestError,
+    }
+  }
+
   return (
     <section className={classes.contact}>
       <h1>留言板</h1>
@@ -61,6 +119,13 @@ function ContactForm() {
           <button>发送</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          message={notification.message}
+          title={notification.title}
+        />
+      )}
     </section>
   )
 }
